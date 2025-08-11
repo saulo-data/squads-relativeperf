78b4879e33683ca3877cd1567e9782085481de85
Saulo Faria <saulo82faria@gmail.com>
2025-08-11 13:17:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ with st.sidebar:
 
     st.subheader("My links (pt-br)")
     st.link_button("Aposta Consciente", "https://apostaconsciente.hotmart.host/product-page-88be95cc-1892-4fa6-b364-69a271150f8f", use_container_width=True)
-    st.link_button("Udemy", "https://www.udemy.com/user/saulo-faria-3/", use_container_width=True)
+    #st.link_button("Udemy", "https://www.udemy.com/user/saulo-faria-3/", use_container_width=True)
     st.link_button("Instagram", "https://www.instagram.com/saulo.foot/", use_container_width=True)
     st.link_button("X", "https://x.com/fariasaulo_", use_container_width=True)
     st.link_button("Youtube", "https://www.youtube.com/channel/UCkSw2eyetrr8TByFis0Uyug", use_container_width=True)
